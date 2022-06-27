ccb974686634e337a6192fa910ed060d2a877c87
IsabellaTrajano <bellrdapct@gmail.com>
2022-06-27 11:22:49 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let paises = ["Brasil", "Mexico", "Japao", "Fiji", "Canada"]
+console.log(paises)
+paises.splice(0, 0, "Franca")
+console.log(paises)
+paises.splice(5, 1)
+console.log(paises)
+paises.splice(5, 0, "Italia", "Australia")
+console.log(paises)
+paises.splice(0, 1)
+console.log(paises)
+
+let numbers = [7,5,6,3,8,9,2,1,4]
+numbers.sort()
+console.log(numbers)
+
